1986cfed5d6e4fd38e74d6906be7d3e12967aead
Kim Boryeong <kbr3551@gmail.com>
2024-12-17 16:09:25 +0900

    fix : 시큐리티 적용 이후 H2 console접속 제한되는 현상 수정

--- Planify/src/main/java/com/planify/main/config/security/SecurityConfig.java
+++ Planify/src/main/java/com/planify/main/config/security/SecurityConfig.java
@@ -16,8 +16,12 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    http
 	        .csrf(csrf -> csrf.disable()) // CSRF는 필요 시 활성화
+	        .headers(headers -> headers
+	                .frameOptions(frameOptions -> frameOptions.disable()) // X-Frame-Options 비활성화
+	        )
 	        .authorizeHttpRequests(auth -> auth
-						.antMatchers(/* "/main", */"/","/login", "/resources/**", "/register", "/api/login/**", "/h2-console/**", "/public/**").permitAll()
+	                .antMatchers("/h2-console/**").permitAll() // H2 콘솔 접근 허용
+	                .antMatchers("/", "/login", "/resources/**", "/register", "/api/login/**", "/public/**").permitAll()
 	            .anyRequest().authenticated()
 	        )
 	        .logout(logout -> logout
