82563301b994d46e3d787f81d4514befc14a7f6a
boryeong <boryeong@RYEONG>
2024-12-21 17:41:36 +0900

    feat : main.js 생성, sql파일 내용 수정, db옵션 변경

--- Planify/src/main/resources/db/data.sql
+++ Planify/src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
 -- data.sql
-insert into member values (1, 'member', 'password', 'temp','kim','M','2024-12-16 14:30:00','2024-12-16 14:30:00');
-insert into member values (2, 'test', '$2a$10$003m6CEo6xjGK8UT.ga9Ye9T8SlQZEiIknepq.yvRMO4rhM3sv1ue', '테스트','테스트','M','2024-12-16 14:30:00','2024-12-16 14:30:00');
+-- insert into member values (1, 'member', 'password', 'temp','kim','M','2024-12-16 14:30:00','2024-12-16 14:30:00');
+insert into member values (1, 'test', '$2a$10$003m6CEo6xjGK8UT.ga9Ye9T8SlQZEiIknepq.yvRMO4rhM3sv1ue', '테스트','테스트','M','2024-12-16 14:30:00','2024-12-16 14:30:00');
---
+++ Planify/src/main/webapp/resources/js/main/main.js
@@ -0,0 +1,18 @@
+/**
+ * main.js
+ */
+$(document).ready(function() {
+    plan.util.init();
+});
+
+if (typeof window.plan === 'undefined') {
+    window.plan = new Object();
+}
+
+window.plan.util = (function() {
+  
+    return {
+		
+    };
+
+})();
